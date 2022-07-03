8a398cfbb817c6d8990d1e565e4e98faa9f95f00
phairot.kanthawichit <p.kanthawichit@gmail.com>
2022-07-03 16:19:43 +0700

    ch1

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
 
+class AddItemPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AddItemPageState();
+  }
+}
+
+class AddItemPageState extends State<AddItemPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Item"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text("hello"),
+        ],
+      ),
+    );
+  }
+}
+
 class MainPage extends StatefulWidget {
   @override
-  State<MainPage> createState() {
+  State<StatefulWidget> createState() {
     return MainPageState();
   }
 }
 
 class MainPageState extends State<MainPage> {
+  showAddItemPage() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return AddItemPage();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Items"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: showAddItemPage,
       ),
       body: Column(
         children: <Widget>[
